8b9288b2b01031b8c5738181d66bacbb729d62e5
Wendy <1600528835@qq.com>
2025-09-02 10:08:28 +0800

    everyday study
    Four day

---
+++ day4.py
@@ -0,0 +1,19 @@
+# 导包
+from pyecharts.charts import Line
+from pyecharts.options import TitleOpts, LegendOpts, ToolboxOpts, VisualMapOpts
+# 创建折线图对象
+line = Line()
+# 添加x轴数据
+line.add_xaxis(["中国", "美国", "英国", "法国", "德国" ])
+# 添加y轴数据
+line.add_yaxis("GDP", [30,20,10,15,25])
+# 设置全局配置项
+line.set_global_opts(
+    title_opts=TitleOpts(title="各国GDP对比", pos_left="center", pos_bottom="1%"),
+    legend_opts=LegendOpts(is_show=True),
+    toolbox_opts=ToolboxOpts(is_show=True),
+    visualmap_opts=VisualMapOpts(is_show=True)
+)
+
+# 生成图表 使用render方法
+line.render()
